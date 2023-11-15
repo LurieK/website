421f85aa9560f63dec4c12d1bc349867c47eaa3a
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-11-15 16:51:04 +0800

    adding client project
    added animation for each section for visuals
    
    added Js for a dropdown for projects

---
+++ scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+
+    const elementsToAnimate = document.querySelectorAll('.opening, .about, .portfolio, .responsive-portfolio, .contact');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                console.log(entry.target.className)
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+
+    
+ });
+
+
+const projects= document.getElementsByClassName('projects')[0];
+
+projects.addEventListener('click', function (){
+    console.log('projects was clicked')
+    const projectsSection = document.querySelector('.images');
+    if (projectsSection.style.display === 'flex') {
+        projectsSection.style.display = 'none';
+    } else {
+        projectsSection.style.display = 'flex';
+    }
+ 
+ })
